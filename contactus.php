fc207154ea3dad79111e84a853f418b8d99f3bd9
itsmeann15 <104128031+itsmeann15@users.noreply.github.com>
2022-05-15 11:16:26 +0800

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -202,8 +202,7 @@
                     
                     <div class="col item social"><a href="https://www.facebook.com/LeoRentACarZamboanga"><i class="icon ion-social-facebook"></i></a></div>
                         <p class="copyright">Maharlika Company © 2022 -  All Rights Reserved</p>
-                    </div>
-            </div>
+                   </div>
             </div>
         </footer>
     </div>    
